2dd6e9eeb5fb1b62e17a40ec438b5a0e9302caf1
subirdas29 <you@example.com>
2022-11-09 01:13:03 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 
+import { RouterProvider } from 'react-router-dom';
 import './App.css';
+import router from './Router/Routes/Routes';
 
 function App() {
   return (
     <div className="App">
-     <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
